cfd8c37ab885e474227657ddc6a3fb7012adfd72
Valeriano A.R <valen.alfonso@gmail.com>
2019-10-28 03:06:36 +0100

    Rect: Remove unused usings.

--- VAR.PdfTools/Maths/Rect.cs
+++ VAR.PdfTools/Maths/Rect.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VAR.PdfTools.Maths
+﻿namespace VAR.PdfTools.Maths
 {
     public class Rect
     {
